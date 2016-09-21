d015c968257c4922a271831ec70af8a2adaf06b5
anshikaa395 <anshikaa395@gmail.com>
2016-09-21 20:06:29 +0200

    added sql for creating database

---
+++ IMS/src/main/resources/DataBaseIMS_V_0_0_1.sql
@@ -0,0 +1,3 @@
+CREATE TABLE userDetails(Id INTEGER, name VARCHAR(55), email VARCHAR(55), telephone INTEGER, PRIMARY KEY(Id));
+
+insert into userDetails values (10001, 'TestUser', 'test@gmail.com', 0686461181);
